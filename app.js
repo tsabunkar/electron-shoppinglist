7510c80e3872b20aebbc80248b5373953ff07334
Tejas Sabunkar <tsabunkar@gmail.com>
2019-05-11 21:21:40 +0530

    add_window

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { app, BrowserWindow, Menu } = electron;
 
 // Outer Window
 let mainWindow;
+let addWindow;
 
 // Listen for the app to be ready
 app.on('ready', () => {
@@ -20,6 +21,12 @@ app.on('ready', () => {
         slashes: true
     })); // This code will generate path ->  'file://dirnamemain_window.html'
 
+
+    // ?Quit all child windows, when main window is closed
+    mainWindow.on('closed', () => {
+        app.quit();
+    })
+
     // !Build menu from the template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     // !Insert menu into mainWindow
@@ -27,6 +34,27 @@ app.on('ready', () => {
 
 });
 
+// create add window
+let createAddShoppingWindow = () => {
+    // Create new window (Browser Window is electron Object)
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item !!'
+    });
+    // load html into window
+    addWindow.loadURL(url.format({
+        // __dirname -> <current_dir>
+        pathname: path.join(__dirname, 'add_window.html'),
+        protocol: 'file:',
+        slashes: true
+    })); // This code will generate path ->  'file://dirnamemain_window.html'
+
+    // Grabage collection handle
+    addWindow.on('close', () => {
+        addWindow = null;
+    });
+}
 
 // !Create Menu template
 // *menu in electron is array of object
@@ -35,7 +63,10 @@ const mainMenuTemplate = [
         label: 'File',
         submenu: [ // Creating submenu for the File Menu 
             {
-                label: 'Add Item'
+                label: 'Add Item',
+                click() {
+                    createAddShoppingWindow();
+                }
             },
             {
                 label: 'Clear Item'
@@ -54,5 +85,30 @@ const mainMenuTemplate = [
         ]
     }
 ];
+
+// In Mac, In menu we see the electron rather than - file, to solve this problem
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({})// addiding empty object in the mainMenuTemplate array  
+}
+
+// ADD developer tools items if not in production
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'DevTool',
+        submenu: [
+            {
+                label: 'Toogle DevTools',
+                accelerator: process.platform == 'drawin' ? 'Command+I' : 'ctrl+I',
+                click(item, focusedWindow) { //focusedWindow -> developer tool whill be showing all the parent and child windows
+                    focusedWindow.toggleDevTools();
+
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
 
 
